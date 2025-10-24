f8eae4baf953ace139e3e8f5096f394e7811373d
Miguel Twahirwa <tmgsr02@gmail.com>
2025-10-23 23:37:41 -0400

    Add Python analysis API and visualization tooling

---
+++ macrobase_py/__init__.py
@@ -0,0 +1,5 @@
+"""MacroBase Python utilities."""
+
+from .api import AnalysisResult, ColumnProfile, PivotView, analyze, analyze_dataframe
+
+__all__ = ["analyze", "analyze_dataframe", "AnalysisResult", "ColumnProfile", "PivotView"]
---
+++ macrobase_py/viz/__init__.py
@@ -0,0 +1,5 @@
+"""Visualization helpers for MacroBase Python."""
+
+from .renderers import plot_pivot_heatmap, plot_top_values, plotly_pivot_table, render_summary_table
+
+__all__ = ["render_summary_table", "plot_top_values", "plot_pivot_heatmap", "plotly_pivot_table"]
